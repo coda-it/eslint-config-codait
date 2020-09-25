84433308613788e83f5dacd21c5e35707a014dff
Oskar Szura <oskar.szura@gmail.com>
2020-09-25 07:47:57 +0200

    CODA-102 - Shareble eslint config (#3)

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "plugins": [
-        "prettier",
-        "flowtype"
-    ],
-    "extends": [
-        "airbnb",
-        "prettier",
-        "plugin:flowtype/recommended"
-    ],
-    "globals": {
-        "window": true,
-        "document": true,
-        "fetch": true,
-        "WebSocket": true,
-        "cy": true,
-        "Cypress": true,
-        "Request": true,
-        "before": true,
-        "FileReader": true,
-    },
-    "env": {
-        "jest": true
-    },
-    "rules": {
-        "no-underscore-dangle": "off",
-        "no-param-reassign": "off",
-        "import/no-extraneous-dependencies": "off"
-    },
-    "settings": {
-        "import/resolver": {
-            "node": {
-                "paths": ["."]
-            }
-        }
+  "parser": "babel-eslint",
+  "plugins": [
+    "prettier",
+    "flowtype"
+  ],
+  "extends": [
+    "airbnb",
+    "prettier",
+    "plugin:flowtype/recommended",
+    "plugin:cypress/recommended",
+    "plugin:react/recommended",
+  ],
+  "globals": {
+    "window": true,
+    "document": true,
+    "fetch": true,
+    "WebSocket": true,
+    "Request": true,
+    "FileReader": true,
+  },
+  "env": {
+    "jest": true
+  },
+  "rules": {
+    "no-underscore-dangle": "off",
+      "no-param-reassign": "off",
+      "import/no-extraneous-dependencies": "off"
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": ["."]
+      }
     }
+  }
 };
